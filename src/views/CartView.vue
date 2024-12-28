4877676891405610f211bf42e5c5a3701782c004
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-28 11:57:39 +0300

    feat: update amount

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -7,19 +7,20 @@
   <a-table :dataSource="cart" :columns="columns">
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex == 'amount'">
-        <a-input :value="record.amount" type="number"></a-input>
+        <a-input v-model:value="record.amount" type="number" @change="handleChangeAmount($el.value,record)"></a-input>
       </template>
 
       <template v-if="column.dataIndex == 'actions'">
         <a-button type="primary" @click="handleRemove(record)">Sil</a-button>
       </template>
     </template>
+    <template #footer><strong>Toplam: {{ cartSum }} $</strong></template>
   </a-table>
 </template>
 
 <script setup lang="ts">
 import supabase from '@/plugin/supaBaseClients';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
 const cart = ref();
 
@@ -37,7 +38,9 @@ const columns = [
     dataIndex: [
       "products",
       "price",
-    ]
+    ],
+    width: "100px",
+
   },
 
   {
@@ -45,7 +48,10 @@ const columns = [
     dataIndex: [
       "products",
       "currency",
-    ]
+
+    ],
+    width: "100px",
+
   },
   {
     title: "Amount",
@@ -74,6 +80,18 @@ const loadData = async () => {
   const userId = await getUserId();
   const { data: cartData } = await supabase.from("carts").select(`id,amount,products(id, price, title, currency)`).eq("user_id", userId);
   cart.value = cartData;
+}
+
+const cartSum = computed(() => {
+  return cart.value?.reduce((acc: number, item: any) => {
+    console.log(item)
+    acc += item.products.price * item.amount;
+    return acc;
+  }, 0);
+})
+
+const handleChangeAmount = async(amount:any,cardItem:any) => {
+  await supabase.from("carts").update({amount:cardItem.amount}).eq("user_id",await getUserId()).eq("id",cardItem.id)
 }
 
 onMounted(async () => {
